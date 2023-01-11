134591fc53dd0bf8cdaaa3f6390fb58caa0c47ed
Rebecca <suttonr1999@gmail.com>
2023-01-11 20:35:03 +0000

    Trying Servo and RFID

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-
 class VideoCamera(object):
     def __init__(self, file_type=".jpg", photo_string="stream_photo"):
         self.vs = PiVideoStream().start()
@@ -41,6 +40,45 @@ class VideoCamera(object):
 app = Flask(__name__)
 pi_camera = VideoCamera()
 
+#Servo Motor Setup
+
+OFFSE_DUTY = 0.5
+SERVO_MIN_DUTY = 2.5+OFFSE_DUTY
+SERVO_MAX_DUTY = 12.5+OFFSE_DUTY
+servoPin = 12
+
+
+def setup():
+    global p
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(servoPin, GPIO.OUT)
+    GPIO.output(servoPin, GPIO.LOW)
+
+    p = GPIO.PWM(servoPin, 50)
+    p.start(0)
+
+
+def servoWrite(angle):
+    if angle < 0:
+        angle = 0
+    elif angle > 180:
+        angle = 180
+
+   p.ChangeDutyCycle(map(angle,0,180,SERVO_MIN_DUTY,SERVO_MAX_DUTY))
+
+
+def setOpen():
+    servoWrite(90)
+
+
+def setClose():
+    servoWrite(0)
+
+
+def destroy():
+    p.stop()
+    GPIO.cleanup()
+
 
 @app.route("/live")
 def live():
@@ -66,7 +104,7 @@ def take_picture():
     return "None"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -76,16 +114,23 @@ def login():
             return redirect(url_for('live'))
     return render_template('login.html', error=error)
 
+@app.route("/")
+def run():
+    setup()
+
+
+    try:
+        while True:
+            id, text = reader.read()
+            if id != 0:
+                setOpen()
+            else:
+                setClose()
+    finally:
+        GPIO.cleanup()
+
+
 
 if __name__ == "__main__":
     app.directory = "./"
     app.run(host="0.0.0.0", port=5000)
-
-    try:
-
-        id, text = reader.read()
-        print(id)
-        print(text)
-
-    finally:
-        GPIO.cleanup()
