16de0068eb716702b93029c73e26cd2b37beffc3
Rebecca <suttonr1999@gmail.com>
2023-01-17 22:29:34 +0000

    Putting terminate joins in

--- main.py
+++ main.py
@@ -92,7 +92,6 @@ def live():
 def generate(camera):
     while True:
         frame = camera.get_frame()
-        print("Grab Image")
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -167,6 +166,9 @@ if __name__ == "__main__":
     q = Process(target=RFID)
 
     q.start()
+    q.join(1)
     p.start()
+    p.join(1)
+
 
 
